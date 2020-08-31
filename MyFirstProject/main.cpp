e8067d399e04d6e735e51c9542c0be5404cd98d7
NorthStarFan <NorthStarFan@users.noreply.github.com>
2020-08-30 19:45:19 -0500

    Initial Commit

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyFirstProject
+//
+//  Created by Justin Stuelke on 8/30/20.
+//  Copyright © 2020 Justin Stuelke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
